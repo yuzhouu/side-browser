ea41e3cacdbea302bcd75edd7d830a515561a764
yuzhou <yuzhou.ini@outlook.com>
2026-09-17 13:40:18 +0800

    feat: add localized website link to help page

--- src/help.ts
+++ src/help.ts
@@ -1,3 +1,8 @@
+import { element } from './dom.js';
 import { localizeDocument } from './i18n.js';
 
 localizeDocument();
+
+if (!document.documentElement.lang.startsWith('zh')) {
+  element<HTMLAnchorElement>('#help-website').href += 'en/';
+}
